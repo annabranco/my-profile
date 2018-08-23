8b65d83431d1c2c25bd5f9742ad52d7d5d6f9f66
Anna <annya.branco@gmail.com>
2018-08-23 21:49:04 +0200

    add: boat navigating 😍
    I'm sooo in love with the boat that if I don't use it on the first page, I'll definitely use it somewhere else.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -15,45 +15,44 @@ class Hero extends React.Component {
 
 			<section className="section__hero container">
 
-				<div className="row">
-					<div className="hero__left col-3">
-						<div className="hero__photo">
-							<img src={Mugshot} alt="Anna Branco" className="hero__mugshot"/>
+					<div className="row">
+						<div className="hero__left col-3">
+							<div className="hero__photo">
+								<img src={Mugshot} alt="Anna Branco" className="hero__mugshot"/>
+							</div>
+						</div>
+						<div className="hero__center col-6">
+							<div className="hero__id">
+								<h1 className="hero__name">Anna Branco</h1>
+								<h2 className="hero__job">{text[language].job}</h2>
+							</div>
+							<div className="hero__aditional">{text[language].aditional}</div>
+						</div>
+						<div className="hero__right col-3">
+							<Social
+								text={text}
+								language={this.props.language}
+							/>
 						</div>
 					</div>
-					<div className="hero__center col-6">
-						<div className="hero__id">
-							<h1 className="hero__name">Anna Branco</h1>
-							<h2 className="hero__job">{text[language].job}</h2>
+					<img className="hero__boat" src="http://www.animatedimages.org/data/media/271/animated-ship-image-0031.gif" alt="Navigating beautifully"/>
+					<div className="hero__scrollDown">
+						<div className="hero__scrollDown-inner">
+							<i class="fas fa-angle-double-down scroll-icon anim1"></i>
+							<i class="fas fa-angle-double-down scroll-icon anim1"></i>
+							<i class="fas fa-angle-double-down scroll-icon anim1"></i>
 						</div>
-						<div className="hero__aditional">{text[language].aditional}</div>
+						<div className="hero__scrollDown-inner">
+							<i class="fas fa-angle-double-down scroll-icon anim2"></i>
+							<i class="fas fa-angle-double-down scroll-icon anim2"></i>
+							<i class="fas fa-angle-double-down scroll-icon anim2"></i>
+						</div>
+						<div className="hero__scrollDown-inner">
+							<i class="fas fa-angle-double-down scroll-icon anim3"></i>
+							<i class="fas fa-angle-double-down scroll-icon anim3"></i>
+							<i class="fas fa-angle-double-down scroll-icon anim3"></i>
+						</div>
 					</div>
-					<div className="hero__right col-3">
-						<Social
-							text={text}
-							language={this.props.language}
-						/>
-					</div>
-				</div>
-				<div className="hero__scrollDown">
-					<div className="hero__scrollDown-inner">
-						<i class="fas fa-angle-double-down scroll-icon anim1"></i>
-						<i class="fas fa-angle-double-down scroll-icon anim1"></i>
-						<i class="fas fa-angle-double-down scroll-icon anim1"></i>
-					</div>
-					<div className="hero__scrollDown-inner">
-						<i class="fas fa-angle-double-down scroll-icon anim2"></i>
-						<i class="fas fa-angle-double-down scroll-icon anim2"></i>
-						<i class="fas fa-angle-double-down scroll-icon anim2"></i>
-					</div>
-					<div className="hero__scrollDown-inner">
-						<i class="fas fa-angle-double-down scroll-icon anim3"></i>
-						<i class="fas fa-angle-double-down scroll-icon anim3"></i>
-						<i class="fas fa-angle-double-down scroll-icon anim3"></i>
-					</div>
-
-				</div>
-
 			</section>
 
 		);
--- src/db/texts.js
+++ src/db/texts.js
@@ -1,34 +1,34 @@
 export default {
 	Header: {
 		en: {
-			title: "Meet Anna Branco's professional profile. Under construction",
+			title: "Meet Anna Branco's professional profile. Under construction.",
 			contact: 'Contact me'
 		},
 		es: {
-			title: "Conoce el perfil profesional de Anna Branco. En construcción",
+			title: "Conoce el perfil profesional de Anna Branco. En construcción.",
 			contact: 'Contactarme'
 		},
 		pt: {
-			title: "Conheça o perfil profissional de Anna Branco. Em construção",
+			title: "Conheça o perfil profissional de Anna Branco. Em construção.",
 			contact: 'Me contatar'
 		},
 	},
 	 Hero: {
 		 en: {
 			 job: 'Front end junior developer',
-			 aditional: 'Aditional content',
+			 aditional: 'Learning to develop sites where navigating around would be a beautiful journey',
 			 call: 'Call',
 			 chat: 'Chat'
 		 },
 		 es: {
 			 job: 'Desarrolladora Front end junior',
-			 aditional: 'Contenido adicional',
+			 aditional: 'Aprendiendo a desarrollar sitios por donde navegar sea un bonita experiencia',
 			 call: 'Llamar',
 			 chat: 'Mensaje'
 		 },
 		 pt: {
 			 job: 'Programadora Front end junior',
-			 aditional: 'Conteúdo adicional',
+			 aditional: 'Aprendendo a desenvolver sites pelos quais navegar seja uma bonita experiência',
 			 call: 'Chamar',
 			 chat: 'Chat'
 		 }
