c89ae3622c6c985252460bd986d6672acb487ccd
Anna <annya.branco@gmail.com>
2018-08-26 12:52:38 +0200

    upd: developer (u/dev)

--- src/components/Developer.js
+++ src/components/Developer.js
@@ -24,121 +24,144 @@ class Developer extends React.Component {
 					<div className="row">
 
 						<div className="developer__sidebar col-3">
-							<h3 className="skills__bar">Main skills</h3>
+							<h3 className="developer__skills--title">Main skills</h3>
 
 							<div className="developer__inner--skills">
 
 								<div className="skills__outer">
 									<h4 className="skills__text">CSS</h4>
-									<img src={css} alt="" className="skills__icon"/>
-									<div className="skills__stars">
-										<i class="fas fa-star icon--star"></i>
-										<i class="fas fa-star icon--star"></i>
-										<i class="fas fa-star icon--star"></i>
+									<div className="skills__inner--icon">
+										<img src={css} alt="" className="skills__icon"/>
+									</div>
+									<div className="skills__inner--stars">
+										<i class="fas fa-star icon--star" aria-hidden></i>
+										<i class="fas fa-star icon--star" aria-hidden></i>
+										<i class="fas fa-star icon--star" aria-hidden></i>
 									</div>
 								</div>
 
 								<div className="skills__outer">
 									<h4 className="skills__text">HTML</h4>
-									<img src={html} alt="" className="skills__icon"/>
-									<div className="skills__stars">
-										<i class="fas fa-star icon--star"></i>
-										<i class="fas fa-star icon--star"></i>
-										<i class="fas fa-star-half icon--star"></i>
+									<div className="skills__inner--icon">
+										<img src={html} alt="" className="skills__icon"/>
+									</div>
+									<div className="skills__inner--stars">
+										<i class="fas fa-star icon--star" aria-hidden></i>
+										<i class="fas fa-star icon--star" aria-hidden></i>
+										<i class="fas fa-star-half icon--star" aria-hidden></i>
 									</div>
 								</div>
 
 								<div className="skills__outer">
 									<h4 className="skills__text">JavaScript</h4>
-									<img src={javascript} alt="" className="skills__icon"/>
-									<div className="skills__stars">
-										<i class="fas fa-star icon--star"></i>
-										<i class="fas fa-star icon--star"></i>
-										<i class="fas fa-star-half icon--star"></i>
+									<div className="skills__inner--icon">
+										<img src={javascript} alt="" className="skills__icon"/>
+									</div>
+									<div className="skills__inner--stars">
+										<i class="fas fa-star icon--star" aria-hidden></i>
+										<i class="fas fa-star icon--star" aria-hidden></i>
+										<i class="fas fa-star-half icon--star" aria-hidden></i>
 									</div>
 								</div>
 
 								<div className="skills__outer">
-									<h4 className="skills__text">SCSS</h4>
-									<i class="fab fa-sass skills__icon icon--sass"></i>
-									<div className="skills__stars">
-										<i class="fas fa-star icon--star"></i>
-										<i class="fas fa-star icon--star"></i>
-										<i class="fas fa-star-half icon--star"></i>
+									<h4 className="skills__text">SASS/SCSS</h4>
+									<div className="skills__inner--icon">
+										<i class="fab fa-sass skills__icon icon--sass" aria-hidden></i>
+									</div>
+									<div className="skills__inner--stars">
+										<i class="fas fa-star icon--star" aria-hidden></i>
+										<i class="fas fa-star icon--star" aria-hidden></i>
+										<i class="fas fa-star-half icon--star" aria-hidden></i>
 									</div>
 								</div>
 
 								<div className="skills__outer">
 									<h4 className="skills__text">React.js</h4>
-									<i class="fab fa-react skills__icon icon--react"></i>
-									<div className="skills__stars">
-										<i class="fas fa-star icon--star"></i>
-										<i class="fas fa-star icon--star"></i>
+									<div className="skills__inner--icon">
+										<i class="fab fa-react skills__icon icon--react" aria-hidden></i>
+									</div>
+									<div className="skills__inner--stars">
+										<i class="fas fa-star icon--star" aria-hidden></i>
+										<i class="fas fa-star icon--star" aria-hidden></i>
 									</div>
 								</div>
 
 								<div className="skills__outer">
 									<h4 className="skills__text">git</h4>
-									<img src={git} alt="" className="skills__icon"/>
-									<div className="skills__stars">
-										<i class="fas fa-star icon--star"></i>
-										<i class="fas fa-star icon--star"></i>
+									<div className="skills__inner--icon">
+										<img src={git} alt="" className="skills__icon"/>
+									</div>
+									<div className="skills__inner--stars">
+										<i class="fas fa-star icon--star" aria-hidden></i>
+										<i class="fas fa-star icon--star" aria-hidden></i>
 									</div>
 								</div>
 
 							</div>
-							<h3 className="skills__bar">Others skills</h3>
-							<div className="developer__inner--skills">
+							<h3 className="developer__skills--title">Others skills</h3>
+							<div className="developer__inner--skills developer__inner--skills-others">
 
 								<div className="skills__outer">
-									<h4 className="skills__text--others">gulp</h4>
-									<i class="fab fa-gulp skills__icon--others icon--gulp"></i>
-									<div className="skills__stars">
-										<i class="fas fa-star-half icon--star icon--star--others"></i>
+									<h4 className="skills__text--others">GitHub</h4>
+									<div className="skills__inner--icon-others">
+										<img src={github} alt="" className="skills__icon--others icon--github"/>
+									</div>
+									<div className="skills__inner--stars">
+										<i class="fas fa-star icon--star icon--star--others" aria-hidden></i>
+										<i class="fas fa-star icon--star icon--star--others" aria-hidden></i>
+										<i class="fas fa-star icon--star icon--star--others" aria-hidden></i>
 									</div>
 								</div>
 
 								<div className="skills__outer">
 									<h4 className="skills__text--others">node.js</h4>
-									<i class="fab fa-node-js skills__icon--others icon--node"></i>
-									<div className="skills__stars">
-										<i class="fas fa-star-half icon--star icon--star--others"></i>
+									<div className="skills__inner--icon-others">
+										<i class="fab fa-node-js skills__icon--others icon--node" aria-hidden></i>
+									</div>
+									<div className="skills__inner--stars">
+										<i class="fas fa-star-half icon--star icon--star--others" aria-hidden></i>
 									</div>
 								</div>
 
 								<div className="skills__outer">
-									<h4 className="skills__text--others">github</h4>
-									<img src={github} alt="" className="skills__icon--others"/>
-									<div className="skills__stars">
-										<i class="fas fa-star icon--star icon--star--others"></i>
-										<i class="fas fa-star icon--star icon--star--others"></i>
-										<i class="fas fa-star icon--star icon--star--others"></i>
+									<h4 className="skills__text--others">gulp</h4>
+									<div className="skills__inner--icon-others">
+										<i class="fab fa-gulp skills__icon--others icon--gulp" aria-hidden></i>
+									</div>
+									<div className="skills__inner--stars">
+										<i class="fas fa-star-half icon--star icon--star--others" aria-hidden></i>
 									</div>
 								</div>
 
 								<div className="skills__outer">
 									<h4 className="skills__text--others">npm</h4>
-									<i class="fab fa-npm skills__icon--others icon--npm"></i>
-									<div className="skills__stars">
-										<i class="fas fa-star-half icon--star icon--star--others"></i>
-
+									<div className="skills__inner--icon-others">
+										<i class="fab fa-npm skills__icon--others icon--npm" aria-hidden></i>
+									</div>
+									<div className="skills__inner--stars">
+										<i class="fas fa-star-half icon--star icon--star--others" aria-hidden></i>
 									</div>
 								</div>
 
 								<div className="skills__outer">
-									<h4 className="skills__text--others">bootstrap</h4>
-									<img src={bootstrap} alt="" className="skills__icon--others"/>
-									<div className="skills__stars">
-										<i class="fas fa-star-half icon--star icon--star--others"></i>
+									<h4 className="skills__text--others">Bootstrap</h4>
+									<div className="skills__inner--icon-others">
+										<img src={bootstrap} alt="" className="skills__icon--others icon--bootstrap" />
+									</div>
+									<div className="skills__inner--stars">
+										<i class="fas fa-star-half icon--star icon--star--others" aria-hidden></i>
 									</div>
 								</div>
 
 								<div className="skills__outer">
-									<h4 className="skills__text--others">zeplin</h4>
-									<img src={zeplin} alt="" className="skills__icon--others"/>
-									<div className="skills__stars">
-										<i class="fas fa-star icon--star icon--star--others"></i>
-										<i class="fas fa-star icon--star icon--star--others"></i>
+									<h4 className="skills__text--others">Zeplin</h4>
+									<div className="skills__inner--icon-others">
+										<img src={zeplin} alt="" className="skills__icon--others icon--zeplin"/>
+									</div>
+									<div className="skills__inner--stars">
+										<i class="fas fa-star icon--star icon--star--others" aria-hidden></i>
+										<i class="fas fa-star icon--star icon--star--others" aria-hidden></i>
 									</div>
 								</div>
 
@@ -147,11 +170,10 @@ class Developer extends React.Component {
 						</div>
 
 							<div className="developer__main col-9">
-								<div className="hero__id">
-									<h1 className="hero__name">Mee Anna Branco</h1>
-									<h2 className="hero__job">{text[language].job}</h2>
+								<div className="developer__formation">
+									<p className="developer__adalab">Formation by: Adalab</p>
 								</div>
-								<div className="developer__proyects">{text[language].aditional}</div>
+								<div className="developer__projects">Some projects of mine</div>
 							</div>
 					</div>
 				</div>
