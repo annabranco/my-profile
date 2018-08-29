790f35218853794d2e04889576613e680d9052e3
Anna <annya.branco@gmail.com>
2018-08-30 01:59:55 +0200

    <Experiences> all languages

--- src/db/texts.js
+++ src/db/texts.js
@@ -82,66 +82,107 @@ export default {
 			goUp: 'subir',
 			showMore: 'ver mais',
 		}
-   },
-   Formation: {
-	   en: {
-		   title: 'Formation',
-		   psy: 'Psychology',
-		   ir: 'International Relations',
-		   master: 'Master in International Relations',
-		   grade: 'Final grade: ',
-		   brazil: 'Brazil',
-		   spain: 'Spain'
-	   },
-	   es: {
-		   title: 'Formación',
-		   psy: 'Psicología',
-		   ir: 'Relaciones Internacionales',
-		   master: 'Master en Relaciones Internacionales',
-		   grade: 'Grado final: ',
-		   brazil: 'Brasil',
-		   spain: 'España'
-	   },
-	   pt: {
-		   title: 'Formação',
-		   psy: 'Psicologia',
-		   ir: 'Relações Internacionais',
-		   master: 'Mestrado em Relações Internacionais',
-		   grade: 'Média final: ',
-		   brazil: 'Brasil',
-		   spain: 'Espanha'
-	   }
-  },
-  Seabed: {
-	  en: {
-		  title: 'Other informations',
-		  message: 'Beyond developing webpages and applications, I love to experiment programming videogames, promoting interactivity with the user and lots of fun.',
-		  messageKeyboard: 'Thus, now you know my basic profile, how about a little gaming experience? If you want more information about previous experiences or other skills, move the scuba diver to the desired direction using arrows keys right or left on your keyboard (◀︎ or ▶︎).',
-		  messageDevices: 'Thus, now you know my basic profile, how about a little gaming experience? If you want more information about previous experiences or other skills, move the scuba diver to the desired direction clicking the directional keys right or left on the corners of the screen.',
-		  experiences: 'Previous experiences',
-		  skills: 'Other skills',
-		  time2go: 'Time to go back...',
-		  me2: 'Yeah. I too would love to explore around, but we have to focus on the professional profile, right?'
-	  },
-	  es: {
-		  title: 'Otras informaciones',
-		  message: 'Además de desarrollar páginas y aplicaciones, me encanta experimentar con la programación de videojuegos, promoviendo interactividad con el usuario de una manera bastante lúdica.',
-		  messageKeyboard: 'Pues, ahora que Usted ya conoce mi perfil básico, qué le parece jugar un poco? Si quiere saber más informaciones acerca de mis experiencias anteriores u otras habilidades, mueve la buceadora para la dirección deseada usando las flechas para la derecha o izquierda del teclado (◀︎ o ▶︎).',
-		  messageDevices: 'Pues, ahora que Usted ya conoce mi perfil básico, qué le parece jugar un poco? Si quiere saber más informaciones acerca de mis experiencias anteriores u otras habilidades, mueve la buceadora para la dirección deseada haciendo click en las flechas direccionales en los cantos de la pantalla para la derecha o izquierda.',
-		  experiences: 'Experiencias anteriores',
-		  skills: 'Otras habilidades',
-		  time2go: 'Es hora de volver...',
-		  me2: 'Ya... a mí también me encantaría explorar más, pero tenemos que focarnos en el perfil profesional, ¿verdad?'
-	  },
-	  pt: {
-		  title: 'Outras informações',
-		  message: 'Além de desenvolver páginas e aplicacões, adoro experimentar com programação de jogos, promovendo interatividade com o usuário de uma maneira bastante lúdica.',
-		  messageKeyboard: 'Assim, agora que você já conhece meu perfil básico, que tal jogar um pouco? Se você quiser saber mais informações sobre minhas experiências anteriores ou outras habilidades, mova a mergulhadora para a direção desejada usando as setas para direita ou esquerda do teclado (◀︎ ou ▶︎).',
-		  messageDevices: 'Assim, agora que você já conhece meu perfil básico, que tal jogar um pouco? Se você quiser saber mais informações sobre minhas experiências anteriores ou outras habilidades, mova a mergulhadora para a direção desejada clicando nas setas direcionais nos cantos da tela para direita ou esquerda.',
-		  experiences: 'Experiências anteriores',
-		  skills: 'Outras habilidades',
-		  time2go: 'É hora de voltar...',
-		  me2: 'Pois é... eu também adoraria explorar mais, mas tenemos que nos focar no perfil profissional, não é?'
-	  }
- },
+   	},
+   	Formation: {
+		en: {
+			title: 'Formation',
+			psy: 'Psychology',
+			ir: 'International Relations',
+			master: 'Master in International Relations',
+			grade: 'Final grade: ',
+			brazil: 'Brazil',
+			spain: 'Spain'
+	   	},
+	   	es: {
+			title: 'Formación',
+			psy: 'Psicología',
+			ir: 'Relaciones Internacionales',
+			master: 'Master en Relaciones Internacionales',
+			grade: 'Grado final: ',
+			brazil: 'Brasil',
+			spain: 'España'
+	   	},
+	   	pt: {
+			title: 'Formação',
+			psy: 'Psicologia',
+			ir: 'Relações Internacionais',
+			master: 'Mestrado em Relações Internacionais',
+			grade: 'Média final: ',
+			brazil: 'Brasil',
+			spain: 'Espanha'
+	   	}
+	},
+	Seabed: {
+		en: {
+			title: 'Other informations',
+			message: 'Beyond developing webpages and applications, I love to experiment programming videogames, promoting interactivity with the user and lots of fun.',
+			messageKeyboard: 'Thus, now you know my basic profile, how about a little gaming experience? If you want more information about previous experiences or other skills, move the scuba diver to the desired direction using arrows keys right or left on your keyboard (◀︎ or ▶︎).',
+			messageDevices: 'Thus, now you know my basic profile, how about a little gaming experience? If you want more information about previous experiences or other skills, move the scuba diver to the desired direction clicking the directional keys right or left on the corners of the screen.',
+			experiences: 'Previous experiences',
+			skills: 'Other skills',
+			time2go: 'Time to go back...',
+			me2: 'Yeah. I too would love to explore around, but we have to focus on the professional profile, right?'
+		},
+		es: {
+			title: 'Otras informaciones',
+			message: 'Además de desarrollar páginas y aplicaciones, me encanta experimentar con la programación de videojuegos, promoviendo interactividad con el usuario de una manera bastante lúdica.',
+			messageKeyboard: 'Pues, ahora que Usted ya conoce mi perfil básico, qué le parece jugar un poco? Si quiere saber más informaciones acerca de mis experiencias anteriores u otras habilidades, mueve la buceadora para la dirección deseada usando las flechas para la derecha o izquierda del teclado (◀︎ o ▶︎).',
+			messageDevices: 'Pues, ahora que Usted ya conoce mi perfil básico, qué le parece jugar un poco? Si quiere saber más informaciones acerca de mis experiencias anteriores u otras habilidades, mueve la buceadora para la dirección deseada haciendo click en las flechas direccionales en los cantos de la pantalla para la derecha o izquierda.',
+			experiences: 'Experiencias anteriores',
+			skills: 'Otras habilidades',
+			time2go: 'Es hora de volver...',
+			me2: 'Ya... a mí también me encantaría explorar más, pero tenemos que focarnos en el perfil profesional, ¿verdad?'
+		},
+		pt: {
+			title: 'Outras informações',
+			message: 'Além de desenvolver páginas e aplicacões, adoro experimentar com programação de jogos, promovendo interatividade com o usuário de uma maneira bastante lúdica.',
+			messageKeyboard: 'Assim, agora que você já conhece meu perfil básico, que tal jogar um pouco? Se você quiser saber mais informações sobre minhas experiências anteriores ou outras habilidades, mova a mergulhadora para a direção desejada usando as setas para direita ou esquerda do teclado (◀︎ ou ▶︎).',
+			messageDevices: 'Assim, agora que você já conhece meu perfil básico, que tal jogar um pouco? Se você quiser saber mais informações sobre minhas experiências anteriores ou outras habilidades, mova a mergulhadora para a direção desejada clicando nas setas direcionais nos cantos da tela para direita ou esquerda.',
+			experiences: 'Experiências anteriores',
+			skills: 'Outras habilidades',
+			time2go: 'É hora de voltar...',
+			me2: 'Pois é... eu também adoraria explorar mais, mas tenemos que nos focar no perfil profissional, não é?'
+		}
+ 	},
+	Experiences: {
+		en: {
+			find: "You've found a curious object laying on the sea bed.",
+			investigate: 'Click on in to investigate it.',
+			ict: 'ICT Instructor',
+			ictDetails: 'I gave classes on Information and Communication Technology skills: windows environment, basic software use and internet. Teaching more than 210 kids of Primary Education, from 6 up to 12 years old.',
+			eru: 'International Emergencies Response Delegate (ERU)',
+			eruDetails: 'I was deployed in Greece on a temporary mission (2 months), providing psychosocial support for Syrian people and other refugees arriving in Europe.',
+			tj: 'Public Servant',
+			tjDetails: 'Public servant with a rising carreer beginning as administrative technician coming to head of sector responsible for social projects.',
+			linkedin: 'For more details ans references, please visit ',
+			brazil: 'Brazil',
+			spain: 'Spain'
+		},
+		es: {
+			find: 'Has encontrado un objecto curioso en el fondo del mar.',
+			investigate: 'Haz click en ello para investigarlo.',
+			ict: 'Instructora de TIC',
+			ictDetails: 'He impartido clases sobre habilidades de Tecnología de la Información y Comunicación: ambiente windows, aplicaciones básicas y uso de internet, enseñando más de 210 niños y niñas de Educación Primaria, de los 6 a los 12 años de edad.',
+			eru: 'Delegada Internacional de Respuesta en Emergencias (ERU)',
+			eruDetails: 'he sido desplegada a Grecia un misión temporal (2 meses), brindando apoyo psicosocial a personas sirias y otros refugiados y refugiadas llegando a Europa.',
+			tj: 'Funcionaria Pública',
+			tjDetails: 'Funcionaria con carrera ascendente, empezando como técnica administrativa y llegando a ser jefe de sector responsable por proyectos sociales.',
+			linkedin: 'Para más detalles y referencias, por favor visite ',
+			brazil: 'Brasil',
+			spain: 'España'
+		},
+		pt: {
+			find: 'Você encontrou um objeto curioso no fundo do mar.',
+			investigate: 'Clique nele para investigá-lo.',
+			ict: 'Instructora de TIC',
+			ictDetails: 'Dei aulas sobre habilidades de Tecnologia de Informação e Comunicação: ambiente windows, softwares básicos e uso de internet, ensinando mais de 210 crianças de Educação Primária, de 6 a 12 anos de idade.',
+			eru: 'Delegada Internacional de Resposta em Emergências (ERU)',
+			eruDetails: 'Trabalhei na Grécia em uma missão temporária (2 meses), propiciando apoio psicossocial para pessoas sírias e outros refugiados e refugiadas chegando à Europa.',
+			tj: 'Servidora Pública',
+			tjDetails: 'Servidora pública com uma carreira ascendente, começando como técnica administrativa e chegando a ser chefe de setor responsável por projetos sociais.',
+			linkedin: 'Para mais informações e referências, por favor visite ',
+			brazil: 'Brasil',
+			spain: 'Espanha'
+		},
+	}
 }
