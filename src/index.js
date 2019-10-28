bbf8efaf88feb01dfb6337a402b618c982ea6789
Anna Branco <annya.branco@gmail.com>
2019-10-28 23:25:02 +0100

    fix: environment variable

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/core/App';
 import {
   TEXTS_PATH_PRE,
   TEXTS_PATH_PRO,
   PROJECTS_PATH_PRE,
   PROJECTS_PATH_PRO
 } from './constants';
-import { App } from './components/core/App';
 import './styles/styles.css';
 
 const APP_VERSION = 'v0.10.0';
 
-const envIsDev = process.env.NODE_ENV === 'development';
-const TEXTS_PATH = envIsDev ? TEXTS_PATH_PRE : TEXTS_PATH_PRO;
-const PROJECTS_PATH = envIsDev ? PROJECTS_PATH_PRE : PROJECTS_PATH_PRO;
+const developWithFakeServer = process.env.REACT_APP_FAKE_SERVER === 'true';
+const TEXTS_PATH = developWithFakeServer ? TEXTS_PATH_PRE : TEXTS_PATH_PRO;
+const PROJECTS_PATH = developWithFakeServer
+  ? PROJECTS_PATH_PRE
+  : PROJECTS_PATH_PRO;
 
 const fetchJson = URL => {
   return fetch(URL)
