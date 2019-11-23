ddc71ad0a438bd3d66fb3302a93c2d6cf40fa1ae
Anna Branco <annya.branco@gmail.com>
2019-11-23 17:51:07 +0100

    upd: fake-server logic using babel-node

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,29 +1,18 @@
-export const TEXTS_PATH_PRO =
-  'https://raw.githubusercontent.com/annabranco/my-profile/feature/technical_review/src/db/texts.json';
+const HOST_PRO =
+  'https://raw.githubusercontent.com/annabranco/my-profile/feature/technical_review/src/db';
+const HOST_PRE = 'http://localhost:3031';
 
-export const PROJECTS_PATH_PRO =
-  'https://raw.githubusercontent.com/annabranco/my-profile/feature/technical_review/src/db/projectsDB.json';
+const isDevelopment = process.env.REACT_APP_FAKE_SERVER === 'true';
+const HOST = isDevelopment ? HOST_PRE : HOST_PRO;
 
-export const SKILLS_PATH_PRO =
-  'https://raw.githubusercontent.com/annabranco/my-profile/feature/technical_review/src/db/skills.json';
+export const TEXTS_PATH = `${HOST}/texts.json`;
 
-export const FORMATION_PATH_PRO =
-  'https://raw.githubusercontent.com/annabranco/my-profile/feature/technical_review/src/db/formation.json';
+export const PROJECTS_PATH = `${HOST}/projectsDB.json`;
 
-export const EXPERIENCES_PATH_PRO =
-  'https://raw.githubusercontent.com/annabranco/my-profile/feature/technical_review/src/db/experiences.json';
+export const SKILLS_PATH = `${HOST}/skills.json`;
 
-export const LANGUAGES_PATH_PRO =
-  'https://raw.githubusercontent.com/annabranco/my-profile/feature/technical_review/src/db/languages.json';
+export const FORMATION_PATH = `${HOST}/formation.json`;
 
-export const TEXTS_PATH_PRE = 'http://localhost:3030/db';
+export const EXPERIENCES_PATH = `${HOST}/experiences.json`;
 
-export const PROJECTS_PATH_PRE = 'http://localhost:3031/db';
-
-export const SKILLS_PATH_PRE = 'http://localhost:3032/db';
-
-export const FORMATION_PATH_PRE = 'http://localhost:3033/db';
-
-export const EXPERIENCES_PATH_PRE = 'http://localhost:3034/db';
-
-export const LANGUAGES_PATH_PRE = 'http://localhost:3035/db';
+export const LANGUAGES_PATH = `${HOST}/languages.json`;
