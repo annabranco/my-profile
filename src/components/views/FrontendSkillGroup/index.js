7ab965cda9c67591491a604a5cdc2e0b9a0f3605
Anna Branco <annya.branco@gmail.com>
2019-12-03 09:05:22 +0100

    add: skills icons to skills view

--- src/components/views/FrontendSkillGroup/index.js
+++ src/components/views/FrontendSkillGroup/index.js
@@ -11,17 +11,48 @@ const FrontendSkillGroup = ({ details }) => (
       {details.skills.map(skill => (
         <SkillWrapper key={skill.skill}>
           <SkillName>{skill.skill}</SkillName>
-          {/*
-                <div className="skills__inner--icon">
-                   <img src={Css} alt="" className="skills__icon" />
-                </div>
-                */}
+
+          {skill.logo && (
+            <div className="skills__inner--icon">
+              <img src={skill.logo} alt="" className="skills__icon" />
+            </div>
+          )}
+
           <SkillLevel level={skill.level} />
         </SkillWrapper>
       ))}
     </FrontendSkillsWrapper>
   </FrontendSkillWrapper>
 );
+
+// const get = () => import('../../../images/skills/js.png');
+
+// const FrontendSkillGroup = ({ details }) => {
+//   // .then(module => module.default)
+//   // .then(path => path);
+//   // console.log('$$$ temp2', temp2);
+//   // return temp2;
+
+//   return get().then(a => (
+//     <FrontendSkillWrapper>
+//       <img src={a.default} alt="" />
+//       <SkillGroupTitle group={details.name}>{details.name}</SkillGroupTitle>
+//       <FrontendSkillsWrapper>
+//         {details.skills.map(skill => (
+//           <SkillWrapper key={skill.skill}>
+//             <SkillName>{skill.skill}</SkillName>
+//             {/*
+//                   <div className="skills__inner--icon">
+//                      <img src={Css} alt="" className="skills__icon" />
+//                   </div>
+//                   */}
+//             <SkillLevel level={skill.level} />
+//           </SkillWrapper>
+//         ))}
+//       </FrontendSkillsWrapper>
+//     </FrontendSkillWrapper>
+//   ));
+// };
 
 FrontendSkillGroup.propTypes = {
   details: PropTypes.string.isRequired
--- src/images/index.js
+++ src/images/index.js
@@ -1,15 +1,26 @@
-export const Bootstrap = require('./skills/bootstrap.png');
-export const Css = require('./skills/css3.png');
 export const FloatingLeft = require('./other/floatingLeft.gif');
 export const FloatingRight = require('./other/floatingRight.gif');
 export const Barquinho = require('./other/barquinho.gif');
-export const Git = require('./skills/git.png');
-export const Github = require('./skills/github.png');
-export const Html = require('./skills/html5.png');
-export const Javascript = require('./skills/js.png');
 export const LogoAdalab = require('./logo-adalab.png');
 export const MacNotebook = require('./bg/bg-mac.jpg');
 export const Mugshot = require('./annabranco.png');
 export const SwimmingRight = require('./other/swimmingRight.gif');
 export const SwimmingLeft = require('./other/swimmingLeft.gif');
-export const Zeplin = require('./skills/zeplin.png');
+
+// Skills
+export const Agile = require('./skills/agile.png');
+export const Angular = require('./skills/angular.png');
+export const Css = require('./skills/css3.png');
+export const Docker = require('./skills/docker.png');
+export const Git = require('./skills/git.png');
+export const Html = require('./skills/html5.png');
+export const Jasmine = require('./skills/jasmine.png');
+export const Javascript = require('./skills/js.png');
+export const Photoshop = require('./skills/photoshop.png');
+export const Python = require('./skills/python.png');
+export const React = require('./skills/react.png');
+export const Redux = require('./skills/redux.png');
+export const Shaka = require('./skills/shaka.png');
+export const StyledComponents = require('./skills/styled-components.png');
+export const Typescript = require('./skills/typescript.png');
+export const Webpack = require('./skills/webpack.png');
