c0387696dba6eea57307d1753019087a318c4e49
Anna Branco <annya.branco@gmail.com>
2021-04-09 23:47:56 +0200

    Updates

---
+++ precache-manifest.1cb0cea7f71e3f7b9736de26b6fca7b8.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fffb727eba95f36690025b563e87086",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "235cf4999bb9e793ace9",
+    "url": "/my-profile/static/css/main.8955e877.chunk.css"
+  },
+  {
+    "revision": "a4640211cd50f08b654f",
+    "url": "/my-profile/static/js/2.f072c919.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/my-profile/static/js/2.f072c919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235cf4999bb9e793ace9",
+    "url": "/my-profile/static/js/main.cd9b048e.chunk.js"
+  },
+  {
+    "revision": "f215d5d8b213cac03d46",
+    "url": "/my-profile/static/js/runtime-main.07fb587a.js"
+  },
+  {
+    "revision": "9afa8fc2af1209a8cc47b0095093e17b",
+    "url": "/my-profile/static/media/agile.9afa8fc2.png"
+  },
+  {
+    "revision": "c346c1ada6d695e80189b43f9ab832b8",
+    "url": "/my-profile/static/media/angular.c346c1ad.png"
+  },
+  {
+    "revision": "dbce07658e68e3d23253d60f98f94dc6",
+    "url": "/my-profile/static/media/annabranco.dbce0765.png"
+  },
+  {
+    "revision": "27c4113da0b9db1999c869e05693d3f9",
+    "url": "/my-profile/static/media/barquinho.27c4113d.gif"
+  },
+  {
+    "revision": "1bff9daf39080fbdfc2b088ab5e7cdd5",
+    "url": "/my-profile/static/media/bg-binary.1bff9daf.jpg"
+  },
+  {
+    "revision": "3dc888b36f67ad67933a774310c96687",
+    "url": "/my-profile/static/media/bg-mac.3dc888b3.jpg"
+  },
+  {
+    "revision": "cfab915769b677d18b8f9c2fa605a5ab",
+    "url": "/my-profile/static/media/css3.cfab9157.png"
+  },
+  {
+    "revision": "7bcbbab5e4c7c9a94f7a32016ae8f827",
+    "url": "/my-profile/static/media/docker.7bcbbab5.png"
+  },
+  {
+    "revision": "0459e19a3d85862675622a1d89671f6c",
+    "url": "/my-profile/static/media/floatingLeft.0459e19a.gif"
+  },
+  {
+    "revision": "96da1471e8b25c510c97e02d9bc72233",
+    "url": "/my-profile/static/media/floatingRight.96da1471.gif"
+  },
+  {
+    "revision": "3eb77fc310d600edd1964170611dfcf6",
+    "url": "/my-profile/static/media/git.3eb77fc3.png"
+  },
+  {
+    "revision": "2060bf3d7eee7b82e1a4d815a9e1b56d",
+    "url": "/my-profile/static/media/html5.2060bf3d.png"
+  },
+  {
+    "revision": "7d1735e1ceaf99f877a94b9bc1a6ae2f",
+    "url": "/my-profile/static/media/jasmine.7d1735e1.png"
+  },
+  {
+    "revision": "5c47f97c885f98c05fb1f6878652685c",
+    "url": "/my-profile/static/media/js.5c47f97c.png"
+  },
+  {
+    "revision": "b406bee1571e30cc9dfc834dad46668a",
+    "url": "/my-profile/static/media/python.b406bee1.png"
+  },
+  {
+    "revision": "43235f13b0f46eb60c1a82506a3cdf84",
+    "url": "/my-profile/static/media/react.43235f13.png"
+  },
+  {
+    "revision": "9babdb88812a01cf3ac96a4e19114826",
+    "url": "/my-profile/static/media/redux.9babdb88.png"
+  },
+  {
+    "revision": "37e848c55d18415f2ad4e501d21b2f5d",
+    "url": "/my-profile/static/media/sea.37e848c5.gif"
+  },
+  {
+    "revision": "4dbe58b53a2a0c18a77bd0394fb18a0f",
+    "url": "/my-profile/static/media/seabed.4dbe58b5.jpg"
+  },
+  {
+    "revision": "38403ebac21d5bc1e83e060da9b6c21e",
+    "url": "/my-profile/static/media/shaka.38403eba.png"
+  },
+  {
+    "revision": "5a1f7181da88eeabe5e1b4ceeac0b18f",
+    "url": "/my-profile/static/media/styled-components.5a1f7181.png"
+  },
+  {
+    "revision": "1d096d35b5d4b8df0ad70a68f0d0fe27",
+    "url": "/my-profile/static/media/swimmingLeft.1d096d35.gif"
+  },
+  {
+    "revision": "d29b979d9b3e30a99949e49b5b06fbdf",
+    "url": "/my-profile/static/media/swimmingRight.d29b979d.gif"
+  },
+  {
+    "revision": "8df0671f76f57c0d63211511e5c9bec3",
+    "url": "/my-profile/static/media/thumbnail-an-adventure.8df0671f.jpg"
+  },
+  {
+    "revision": "2e48f335400bcc73a0e2d700590ae4e2",
+    "url": "/my-profile/static/media/thumbnail-days-go-by.2e48f335.jpg"
+  },
+  {
+    "revision": "0d94b86b1752070566efaeba11180b8f",
+    "url": "/my-profile/static/media/thumbnail-harry-potter-database.0d94b86b.jpg"
+  },
+  {
+    "revision": "08dd797a64a8bb869841562d186590d2",
+    "url": "/my-profile/static/media/thumbnail-my-profile.08dd797a.jpg"
+  },
+  {
+    "revision": "39329ba02d436565e1b4bc07929d558f",
+    "url": "/my-profile/static/media/thumbnail-no-name-kitchen.39329ba0.jpg"
+  },
+  {
+    "revision": "91f19d61897f319b8817bc9f70613dc8",
+    "url": "/my-profile/static/media/thumbnail-shougi.91f19d61.jpg"
+  },
+  {
+    "revision": "4054b331324a2f06b443c37c0472d04d",
+    "url": "/my-profile/static/media/thumbnail-toy-story.4054b331.jpg"
+  },
+  {
+    "revision": "57f937816bdd766990500b072d6d1882",
+    "url": "/my-profile/static/media/thumbnail-tv-series-finder.57f93781.jpg"
+  },
+  {
+    "revision": "643fb6d032e24f561a19b45d2eac7157",
+    "url": "/my-profile/static/media/thumbnail-zombicide-party.643fb6d0.jpg"
+  },
+  {
+    "revision": "b36ddcc217227fae1d9cf414a9e89b87",
+    "url": "/my-profile/static/media/typescript.b36ddcc2.png"
+  },
+  {
+    "revision": "581fe04b6429ec8413e9415fcb7c1246",
+    "url": "/my-profile/static/media/webpack.581fe04b.png"
+  }
+]);
